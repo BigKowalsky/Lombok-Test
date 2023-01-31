f1dbd6b584c9c17ada4c2bdbb5d72ae5b5ea14c1
Tomasz Kowalski <tomek.kowalski.89@gmail.com>
2023-01-31 20:58:34 +0100

    23.3 Mail to CC fix

--- src/main/java/com/crud/tasks/service/SimpleEmailService.java
+++ src/main/java/com/crud/tasks/service/SimpleEmailService.java
@@ -8,6 +8,8 @@ import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Slf4j
 @Service
 @RequiredArgsConstructor
@@ -31,7 +33,9 @@ public class SimpleEmailService {
         mailMessage.setTo(mail.getMailTo());
         mailMessage.setSubject(mail.getSubject());
         mailMessage.setText(mail.getMessage());
-        if(mail.getMailToCC() != null) mailMessage.setCc(mail.getMailToCC());
+        if (Optional.ofNullable(mail.getMailToCC()).isPresent()) {
+            mailMessage.setCc(mail.getMailToCC());
+        }
         return mailMessage;
     }
 }
